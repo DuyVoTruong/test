79b0d0ec19cc03202392ee0c6c98e697a7ca4930
VoDuy89123 <votruongduy1943@gmail.com>
2023-02-07 11:29:42 +0700

    finish project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import AppTest from './component/AppTest';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/component/AppTest.js
+++ src/component/AppTest.js
@@ -1,10 +1,33 @@
-import {testFunction} from "../api/test-modul"
+import { useEffect } from "react";
+import { Form } from "react-bootstrap";
+import {testFunction, getTinhThanhPho} from "../api/test-modul"
 const AppTest =()=>{
+
+    useEffect(()=>{
+        getTinhThanhPho();
+    },[])
+
     return(
         <>
         {
             testFunction()
         }
+        <Form.Group className="mb-3" controlId="formDiaChi">
+            <Form.Label>Địa chỉ</Form.Label>
+            <Form.Select className="mb-3" id="city" aria-label="Chọn tỉnh thành">
+                <option>Chọn tỉnh thành</option>
+            </Form.Select>
+            
+            <Form.Select className="mb-3" id="district" aria-label="Chọn quận huyện">
+                <option>Chọn quận huyện</option>
+            </Form.Select>
+                    
+            <Form.Select className="mb-3" id="ward" aria-label="Chọn phường xã">
+                <option>Chọn phường xã</option>
+            </Form.Select>
+
+            <Form.Control type="text" placeholder="Nhập số nhà" />
+        </Form.Group>
         </>
     )
 }
